0264f485b924a790f9a64f905f020af9cdf106c0
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2024-07-09 20:30:16 -0600

    hacemos input validation para el numero que ingresamos
    <index.html>
    el boton 'nuevo juego' ahora se puede usar y usa la funcion 'erase'
    
    <app.js>
    el numero secreto se mantiene como constante y el que ingresamos
    una variable.
    
    usamor una comprobacion, si el numero es >10 o <1 o no existe borramos
    ese numero, y si lo es mostramos el numero
    
    tambien agregamos la funcion erase() para borrar el contenido del input

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let secret_num = Math.floor((Math.random() * 10)) + 1;
+const secret_num = Math.floor((Math.random() * 10)) + 1;
+let user_num;
 
-function pushPop(){
-    let user_num = parseInt(document.querySelector("#valor_usuario").value);
-    alert(user_num);
+function play(){ // funcion principal
+    user_num = parseInt(document.querySelector("#valor_usuario").value); // obtenemos el valor del input
+
+    if (user_num > 10 || user_num < 1 || !user_num) { // revisamos si esta en nuestro rango
+        erase(); // si no lo esta, borramos este numero, por lo tanto no existe
+        alert("Ingresa un numero valido entre 1 y 10");
+
+    } else {
+        alert(user_num);
+    }
+}
+
+function erase(){
+    const input  = document.querySelector("#valor_usuario");
+    input.value = "";
 }
