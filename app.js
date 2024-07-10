be897ce8fdcfa3d6994482bf0c2a1e9872eba4f1
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2024-07-10 16:58:34 -0600

    app.js comprueba que el numero secreto es igual al numero de usuario
    <app.js>
    dentro de la funcion *play* ahora creamos validaciones para comprobar si el numero
    secreto y el del usuario sean el mismo, de lo contrario, mostramos diferentes
    alertas.
    
    *erase()*
    ahora la funcion en vez de solo borrar, tambien crea un numero numero secreto, para
    que el juego pueda volver a empezar

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const secret_num = Math.floor((Math.random() * 10)) + 1;
+let secret_num = Math.floor((Math.random() * 10)) + 1;
 let user_num;
 
 function play(){ // funcion principal
     user_num = parseInt(document.querySelector("#valor_usuario").value); // obtenemos el valor del input
 
     if (user_num > 10 || user_num < 1 || !user_num) { // revisamos si esta en nuestro rango
-        erase(); // si no lo esta, borramos este numero, por lo tanto no existe
+        // si no lo esta, borramos este numero, por lo tanto no existe
+        const input  = document.querySelector("#valor_usuario");
+        input.value = "";
         alert("Ingresa un numero valido entre 1 y 10");
 
     } else {
-        alert(user_num);
+        // alert(user_num);
+        if (user_num == secret_num){
+            alert("Felicidades, atinaste el numero");
+        } else if (user_num > secret_num){
+            alert("Lo siento, el numero es menor");
+        } else {
+            alert("Lo siento, el numero es mayor")
+        }
     }
 }
 
 function erase(){
+    secret_num = Math.floor((Math.random() * 10)) + 1;
+    alert("El numero secreto ha sido cambiado")
     const input  = document.querySelector("#valor_usuario");
     input.value = "";
 }
