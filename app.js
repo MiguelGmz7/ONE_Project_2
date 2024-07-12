0e0ffcb72dee63a0d7a47899b385f058bef0a0d1
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2024-07-11 19:24:18 -0600

    El numero secreto no se puede repetir y se agrega a una lista
    Ahora necesitamos que el codigo no repita el numero secreto, por lo que tenemos que guardar estos
    numeros en una lista.
    
    <app.js>
    creamos la funcion *genNumSecreto*, la cual es una funcion recursiva, esta nos permite crear un
    numero aleatorio, si este ya esta en la lista, volvemos a usar la funcion, de lo contrario lo
    regresamos
    
    *printDrawnNumbers*:
    esta funcion nos permite imprimir los numeros sortedos dentro del espacio 'numeros sorteados'
    es importante mencionar que solo la usamos despues de que el usuario adivino el numero
    
    *printParagraph*:
    funcion que nos ayuda a cambiar el *TEXTO Y EL COLOR* del parrafo del juego
    
    *boton.removeAtribute y boton.setAtribute*:
    habilitamos o desabilitamos el boton para forzar al usuario a que presionar el boton de
    nuevo juego, gracias a esto, podemos resetear el numero secreto cada juego
    
    <index.html>
    creamos un nuevo apartado en el div de pruebas
    
    <styles.css>
    LN_41: agregamos 'margin-top' que nos permite alejar todo el recuadro de los h1

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let secret_num = Math.floor((Math.random() * 10)) + 1; // tenemos un valor aletorio entre 1 y 10
 
 let user_num;
 
 let counter = document.querySelector("#counter"); // obtenemos el counter (ahora 0)
 
+
+let list = [];
+
+let play_num = genNumSecreto();
+
+let boton = document.querySelector("#nuevo");
+
 function play(){ // funcion principal
     user_num = parseInt(document.querySelector("#valor_usuario").value); // obtenemos el valor del input
+    console.log(play_num); //revisar el contenido de la lista
+    console.log(list); 
 
     if (user_num > 10 || user_num < 1 || !user_num) { // revisamos si esta en nuestro rango
 
@@ -17,14 +25,17 @@ function play(){ // funcion principal
     } else {
         // aqui comprobamos si el numero secreto es igual al numero de usuario
 
-        if (user_num == secret_num){
-            alert("Felicidades, atinaste el numero");
-        } else if (user_num > secret_num){
-            alert("Lo siento, el numero es menor");
+        if (user_num == play_num){
+            printParagraph("Felicidades, atinaste el numero", "gold");
+            printDrawnNumbers();
+            boton.setAttribute('disabled','true');
+        } else if (user_num > play_num){
+            printParagraph("Lo siento, el numero es menor", "red");
             //counter.textcontent nos permite modificar el contador (ahora 0) y sumarle un intento mas
             counter.textContent = parseInt(counter.textContent) + 1;
+
         } else {
-            alert("Lo siento, el numero es mayor")
+            printParagraph("Lo siento, el numero es mayor", "blue");
             counter.textContent = parseInt(counter.textContent) + 1;
         }
     }
@@ -32,9 +43,37 @@ function play(){ // funcion principal
 
 function erase(){
     // esta opcion reinicia el juego 
-    secret_num = Math.floor((Math.random() * 10)) + 1;
     alert("El numero secreto ha sido cambiado")
     const input  = document.querySelector("#valor_usuario");
     input.value = "";
     counter.textContent = 0;
+    printParagraph("Indica un número secreto entre 1 y 10", "white");
+    play_num = genNumSecreto();
+    boton.removeAttribute('disabled');
+}
+
+function genNumSecreto(){
+    let secret_num = Math.floor((Math.random() * 10)) + 1; // tenemos un valor aletorio entre 1 y 10
+    if (list.includes(secret_num)) {
+        return genNumSecreto();
+    } else {
+        list.push(secret_num);
+        return secret_num;
+    }
+}
+
+function printDrawnNumbers(){
+    let drawnlist = document.querySelector("#list");
+    let string = "";
+    
+    for (i = 0; i < list.length; i++){
+        string += list[i].toString() + ", ";
+    }
+    drawnlist.textContent = string;
+}
+
+function printParagraph(string, color){
+    let p = document.querySelector(".texto__parrafo");
+    p.textContent = string;
+    p.style.color = color; //nos ayuda a cambiar el color 
 }
